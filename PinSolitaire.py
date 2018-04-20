c71ad4d1a2ad602b048b5e463064b6507b689556
PoPeduggan <34069786+PoPeduggan@users.noreply.github.com>
2018-04-20 13:58:47 +0100

    Update PinSolitaire.py

--- PinSolitaire.py
+++ PinSolitaire.py
@@ -3,9 +3,20 @@ import tkinter as tk
 root=tk.Tk()
 
 btn=[[0 for x in range(7)]for y in range(7)]
+highscoreTable = [["" for x in range (0, 2)]for y in range(0, 10)]
 
 start = tk.Button(root, text = "Start", command = lambda : drawBoard())
 start.grid(column=5, row=30, pady=10,padx=10)
+
+with open ("users.txt","r") as f:
+            for x in range (10):
+                highscoreTable[x][0] = f.readline()
+                highscoreTable[x][1] = f.readline()
+                nameEntry[x] = tk.Label(text = highscoreTable[x][0][:-1])
+                nameEntry[x].grid(column = 7, row = 17+x, padx = 1, pady = 1 )
+                scoreEntry[x] = tk.Label(text = highscoreTable[x][1][:-1])
+                scoreEntry[x].grid(column = 13, row = 17+x, padx = 1, pady = 1 )
+        f.close()
 
 def drawBoard():
 
@@ -25,8 +36,14 @@ def drawBoard():
     for x in range(16):
         lst_xval[x].config(text = "X")
 
+    Wquit = tk.Button(root, text = "Finish", command = lambda: endGame())
+    Wquit.grid(column = 6, row = 12)
+
     start.grid_remove()
 
+    for x in range(10):
+        nameEntry[x].grid_remove()
+        scoreEntry[x].grid_remove()
 def toggle(x, y):
 
     if x-2 >= 0 and btn[x-2][y].cget("text") == "0" and btn[x-1][y].cget("text") == "1":
@@ -138,4 +155,76 @@ def jump(x, y, direction):
         btn[x][y].config(text = "0", command ="")
         btn[x][y-2].config(text = "1", command =lambda xVar= x, yVar=(y-2): toggle(xVar, yVar))
         btn[x][y-1].config(text = "0", command ="")
+
+def endGame():
+    
+    score=0
+    for x in range(7):
+        for y in range(7):
+            if btn[x][y].cget("text") == "1":
+                score = score + 1
+
+    validate = False
+
+    while validate == False:
+        name = simpledialog.askstring("Well Done", "Your score is %s. PLease enter a name to be placed on the leaderboard." % score)
+        if name == "":
+            messagebox.showerror("Error!", "Please enter a name.")
+        else:
+            validate = True
+    highscoreTable = [["100" for x in range (0, 2)]for y in range(0, 11)]
+
+    highscoreTable[10][0] = name
+    highscoreTable[10][1] = score
+
+    with open ("users.txt","r") as f:
+        for x in range (10):
+            highscoreTable[x][0] = f.readline()
+            highscoreTable[x][1] = f.readline()
+            highscoreTable[x][0] = highscoreTable[x][0][:-1]
+            highscoreTable[x][1] = highscoreTable[x][1][:-1]
+            highscoreTable[x][1] = int(highscoreTable[x][1])
+    f.close()
+
+    swap = True
+    times = len(highscoreTable) - 1
+
+    while times > 0 and swap == True:
+       swap = False
+       for position in range(times):
+           if highscoreTable[position][1] > highscoreTable[position + 1][1]:
+               swap = True
+               temp = highscoreTable[position][1]
+               temp2 = highscoreTable[position][0]
+               highscoreTable[position][0] = highscoreTable[position + 1][0]
+               highscoreTable[position][1] = highscoreTable[position + 1][1]
+               highscoreTable[position + 1][1] = temp
+               highscoreTable[position + 1][0] = temp2
+       times = times - 1
+
+    for x in range(11):
+        highscoreTable[x][0] = highscoreTable[x][0] +"\n"
+        highscoreTable[x][1] = str(highscoreTable[x][1]) + "\n"
+
+    with open ("Users.txt","w") as f:
+        for x in range (10):
+            f.write(highscoreTable[x][0])
+            f.write(highscoreTable[x][1])
+    f.close()
+
+    for x in range(7):
+        for y in range(7):
+            btn[x][y].grid_remove()
+    
+    Wquit.grid_remove()
+    
+    start.grid()
+
+    for x in range (10):
+            nameEntry[x].grid()
+            nameEntry[x].config(text=highscoreTable[x][0])
+            scoreEntry[x].grid()
+            scoreEntry[x].config(text=highscoreTable[x][1])
+
+    updater = True
 root.mainloop()
